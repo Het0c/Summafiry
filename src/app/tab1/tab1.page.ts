5ce30ad1ff10d208659c529251eec5fd210f4197
DATE-KIPURA <di.aravenah@duocuc.cl>
2024-09-11 01:28:04 -0300

    Diseño V2 beta

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,13 +7,20 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  searchVisible: boolean = false; // Controla la visibilidad del campo de búsqueda
+  searchQuery: string = '';       // Almacena el valor de búsqueda
 
   constructor(private alertController: AlertController) {}
 
-  onSearch(event: any) {
-    const value = event.target.value;
+  // Alterna la visibilidad del campo de búsqueda
+  toggleSearch() {
+    this.searchVisible = !this.searchVisible;
+  }
+
+  // Maneja los cambios en el campo de búsqueda
+  onSearchChange(event: any) {
+    const value = event.detail.value;  // Obtén el valor del campo de búsqueda
     console.log('Valor de búsqueda:', value);
     // Aquí puedes agregar la lógica para filtrar o buscar datos
   }
-  
 }
